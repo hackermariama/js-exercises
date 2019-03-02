6972bc38bda3e9bb223b994374cfbc9a20d81051
Mariama <mariamasillah81@gmail.com>
2019-03-02 23:13:01 +0000

    Use the trim method to remove extra whitespace I did this because it was required by the question.

--- week-1/G-strings-methods/exercise2.js
+++ week-1/G-strings-methods/exercise2.js
@@ -1,3 +1,10 @@
-var name = " Daniel  ";
+var name = "mariama";
+var namelength = name.length;
+console.log(
+  "my name is " + name + " and my name is " + namelength + " characters long"
+);
 
-console.log(message);
+function myFunction() {
+  var str = "     my name is mariama and my name is 7 characters long!     ";
+  alert(str.trim());
+}
