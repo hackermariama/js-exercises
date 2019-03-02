27cac0256019fa404bd06d797471fe379e4a569e
Mariama <mariamasillah81@gmail.com>
2019-03-02 23:42:28 +0000

    create two variables and log a message total no numbers of each. I did this because it was required by the question H.

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,1 +1,7 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var NumberOfStudents = 15;
+var NumberofMentors = 8;
+var sum = 15 + 8;
+console.log("Number Of Students " + NumberOfStudents);
+console.log("Number of Mentors " + NumberofMentors);
+console.log("Total no of Students and Mentors " + sum);
