7f3c1de7852e851b5cc895c31a69f8b75ce00fd5
Mariama <mariamasillah81@gmail.com>
2019-03-02 22:20:21 +0000

    Print a message with a greeting and my name. I did this because it was required by question.

--- week-1/F-strings-concatenation/exercise.js
+++ week-1/F-strings-concatenation/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `name`
+var greetingStart = "hello my nama is ";
+var name = "mariama";
+var greeting = greetingStart + name;
 
-console.log(name);
+console.log(greeting);
