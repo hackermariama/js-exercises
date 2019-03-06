db154a703306150a98810a72ba9ed15daac89b09
Mariama <mariamasillah81@gmail.com>
2019-03-06 20:31:16 +0000

    complete I-floats exercise

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,16 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var precisePercentageStudents =
+  (numberOfStudents / (numberOfStudents + numberOfMentors)) * 100;
+var percentageStudents = Math.round(precisePercentageStudents);
+
+var precisePercentageMentors =
+  (numberOfMentors / (numberOfMentors + numberOfStudents)) * 100;
+var precisePercentageMentors = Math.round(precisePercentageMentors);
+
+console.log(
+  "percentage students: " + Math.round(precisePercentageStudents) + "%"
+);
+console.log(
+  "percentage mentors: " + Math.round(precisePercentageMentors) + "%"
+);
